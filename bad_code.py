929e6f4c636704db65a4e22ca5edd797c8ebec87
Shiv Patel <srp2762@gmail.com>
2026-09-16 19:28:29 -0400

    commit

--- bad_code.py
+++ bad_code.py
@@ -8,3 +8,17 @@ def thing(a, b, c):
         return z
     else:
         return a + b
+
+def stuff(a, b):
+    x = 0
+    x = a
+    x = x + b
+    print("The answer is:", x)
+
+
+def calculate(a, b, operation):
+    if operation == "add":
+        result = 0
+        result = a
+        result = result + b
+        print("The answer is:", result)
